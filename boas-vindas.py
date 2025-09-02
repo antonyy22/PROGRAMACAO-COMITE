21fde860bd5dafcf1dedeb6f0f49559e866079ea
antonyy22 <puginskiantony@gmail.com>
2025-09-02 20:48:43 -0300

    primeiro commit

---
+++ boas-vindas.py
@@ -0,0 +1,12 @@
+# Coleta das variaveis
+nome= input ("informe o seu nome -> ")
+idade = int (input ("Informe a sua idade -> "))
+
+# Mensagem de boas vindas
+print ("Olá", nome, "Seja bem vindo!")
+
+# Calculando a idade futura
+idade_futura = idade + 5
+
+# Apresentar idade futura
+print("A sua idade daqui 5 anos será -> ", idade_futura )
---
+++ calculos.py
@@ -0,0 +1,10 @@
+numero_1 =int (input("Informe o primeiro numero -> " ))
+numero_2 = int (input("Informe o segundo numero -> " ))
+nome = input ("Informe seu nome -> ")
+#soma = numero_1 + numero_2
+#print("A soma é ->", soma )
+
+print("A soma dos numeros é ->", numero_1 + numero_2 )
+print (nome, nome, nome)
+print ("O resto da divisão é -> ", numero_1 % numero_2 )
+print ("A potencia de", numero_1, "Elevado em", numero_2, "é -> ", numero_1 ** numero_2)
